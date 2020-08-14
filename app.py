8b27bc5f7d1d2956dc866c48be406d4d16b69425
Belinda Boudra <belinda.boudra@epitech.eu>
2020-08-14 16:41:06 +0200

    last push :'(

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask, jsonify
 import json
 
+from flask import Flask, jsonify
+from flask_cors import cross_origin
 
 app = Flask(__name__)
 
 
-@app.route("/api/meteo")
-def meteo():
+@app.route("/api/cars", methods=['GET'])
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
+def cars():
     with open('json/cars.json') as f:
         data = json.load(f)
         return jsonify(data)
---
+++ frontend/carfront/src/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>Home page</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  props: ['FIELD1'],
+  data () {
+    return {
+      cars: ''
+    }
+  },
+  mounted () {
+    axios
+      .get('http://127.0.0.1:5000/api/cars')
+      .then(response => {
+        this.cars = response.data
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+}
+</script>
+
